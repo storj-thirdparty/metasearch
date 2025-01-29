e1b68fbd63882ed16abb38680453645ca83f0bcd
Akos Gyimesi <akos@integrated-digital.com>
2025-01-29 14:39:06 +0100

    Add LICENSE file and copyright headers

--- sql/001.objects.clear_metadata.sql
+++ sql/001.objects.clear_metadata.sql
@@ -1,3 +1,5 @@
+-- Copyright (C) 2025 Storj Labs, Inc.
+-- See LICENSE for copying information.
 ALTER TABLE objects ADD COLUMN IF NOT EXISTS clear_metadata JSONB;
 CREATE INDEX IF NOT EXISTS objects_clear_metadata_idx ON objects USING GIN (clear_metadata);
 COMMENT ON COLUMN objects.clear_metadata is 'clear_metadata contains unencrypted metadata that indexed for efficient metadata search.';
