1dc377d65ed8642260a4095a6d857cdd9cb8ef17
Akos Gyimesi <akos@integrated-digital.com>
2025-02-07 13:01:46 +0100

    make SQL migration scripts idempotent

--- sql/002.object_metadata_changes.sql
+++ sql/002.object_metadata_changes.sql
@@ -2,11 +2,11 @@
 -- See LICENSE for copying information.
 
 ALTER TABLE objects
-    ADD COLUMN metasearch_queued_at TIMESTAMP
+    ADD COLUMN IF NOT EXISTS metasearch_queued_at TIMESTAMP
     DEFAULT current_timestamp()
     ON UPDATE current_timestamp();
 
-CREATE INDEX objects_metasearch_queued_at_idx ON objects (
+CREATE INDEX IF NOT EXISTS objects_metasearch_queued_at_idx ON objects (
     project_id,
     metasearch_queued_at
 ) WHERE metasearch_queued_at IS NOT NULL;
