acef6a9e50d5379c306e8ef3ee4abd5ad65bc5ba
Akos Gyimesi <akos@integrated-digital.com>
2025-02-07 13:04:16 +0100

    Move SQL migrations into a single script

--- sql/001.objects.clear_metadata_metasearch_queued_at.sql
+++ sql/001.objects.clear_metadata_metasearch_queued_at.sql
@@ -15,3 +15,13 @@ CREATE INDEX IF NOT EXISTS objects_metasearch_queued_at_idx ON objects (
     metasearch_queued_at
 ) WHERE metasearch_queued_at IS NOT NULL;
 
+ALTER TABLE objects
+    ADD COLUMN IF NOT EXISTS metasearch_queued_at TIMESTAMP
+    DEFAULT current_timestamp()
+    ON UPDATE current_timestamp();
+
+CREATE INDEX IF NOT EXISTS objects_metasearch_queued_at_idx ON objects (
+    project_id,
+    metasearch_queued_at
+) WHERE metasearch_queued_at IS NOT NULL;
+
